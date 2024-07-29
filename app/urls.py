fc5676a01c380c1c258aafedf56acd44aca64d72
loboprogrammingg <matheusloboo2001@gmail.com>
2024-07-29 18:26:03 -0400

    Adicionando o Update, Delete, Retrieve

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import GenreCreateListView
+from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view')
 ]
