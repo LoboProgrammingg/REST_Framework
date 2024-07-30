b2ccecd063d179de8ce7fff94dbeee4acab38371
loboprogrammingg <matheusloboo2001@gmail.com>
2024-07-30 16:30:56 -0400

    Criando o admin do app movies

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from movies.models import Movie
 
-# Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'genre', 'release_date', 'resume')
