d0cd7f961be503e45bc5fabf94675d35a06458c0
loboprogrammingg <matheusloboo2001@gmail.com>
2024-07-29 20:02:29 -0400

    Adicionando o Actor ao admin.py

--- actors/admin.py
+++ actors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from actors.models import Actor
 
-# Register your models here.
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'birthday', 'nationality')
