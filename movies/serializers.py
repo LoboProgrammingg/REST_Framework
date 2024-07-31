fb0ecd6fc31c03e8220d51404569274adf01d149
loboprogrammingg <matheusloboo2001@gmail.com>
2024-07-31 17:27:11 -0400

    Criando uma media com base nos reviews enviado pelos usuarios

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,19 +1,30 @@
 from rest_framework import serializers
+from django.db.models import Avg
 from movies.models import Movie
 
 
 class MovieModelSerializer(serializers.ModelSerializer):
 
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
 
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+        
+        if rate:
+            return round(rate, 1)
+        
+        return None
+
     def validate_release_date(self, value):
-        if value.year < 1970:
-            raise serializers.ValidationError('A data de lancamento nao pode ser anterior a 1970.')
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lancamento nao pode ser anterior a 1900.')
         return value
     
     def validate_resume(self, value):
-        if len(value) > 200:
-            raise serializers.ValidationError('campo de resumo nao pode ter mais de 200 caracteres.')
+        if len(value) > 500:
+            raise serializers.ValidationError('campo de resumo nao pode ter mais de 500 caracteres.')
         return value
