d85a7a28cf86ff1fcc09f1a2d60b3e3104e49804
loboprogrammingg <matheusloboo2001@gmail.com>
2024-08-01 16:54:23 -0400

    Ultilizando JWT para seguranca do servidor app criado authentication

--- app/settings.py
+++ app/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'rest_framework_simplejwt',
     
+    'authentication',
     'genres',
     'actors',
     'movies',
