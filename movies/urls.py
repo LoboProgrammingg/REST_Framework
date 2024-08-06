d64f59dd5bdd8af19a7ba786fcfbc086b80134b7
loboprogrammingg <matheusloboo2001@gmail.com>
2024-08-06 15:03:02 -0400

    Adicionando endpoint de estatistica em movies

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
+from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView, MovieStatsView
 
 
 
 urlpatterns = [
     path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
     path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
+    path('movies/stats/', MovieStatsView.as_view(), name='movie-stats-view'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics
+from django.db.models import Count, Avg
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermission
 from movies.models import Movie
+from reviews.models import Review
 from movies.serializers import MovieModelSerializer
 
 
@@ -14,3 +16,22 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
+
+    def get(self, request):
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values(
+            'genre__name').annotate(count=Count('id'))
+        total_reviews = Review.objects.count()
+        average_stars = Review.objects.aggregate(
+            avg_stars=Avg('stars'))['avg_stars']
+
+        return response.Response(data={
+            'total_movies': total_movies,
+            'movie_by_genre': movies_by_genre,
+            'total_reviews': total_reviews,
+            'average_stars': round(average_stars, 1) if average_stars else 0,
+        }, status=status.HTTP_200_OK,)
